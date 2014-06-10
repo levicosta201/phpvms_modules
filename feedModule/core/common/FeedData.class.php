a62d56d41961a2a5f9746c7ae6e30b0c03d8980f
Levi <levi.costa1@gmail.com>
2014-06-10 14:57:48 -0300

    twMerco CEO

--- feedModule/core/common/FeedData.class.php
+++ feedModule/core/common/FeedData.class.php
@@ -1,15 +1,15 @@
 <?php
 
  /***************************************************************************************	
- *											*	
- *											*
- * twMerco is licenced Creative Commons:  						*
+ *																						*	
+ *																						*
+ * twMerco is licenced Creative Commons:  												*
  * View license.txt in the root, or visit http://creativecommons.org/licenses/by/4.0/	*
- *											*
- * @author Levi Costa									*
- * @copyright Copyright (c) 2014, Mercosul Network Devlopement				*
- * @link http://www.voemercosul.com/network/br						*
- * @license http://creativecommons.org/licenses/by/4.0/					*
+ *																						*
+ * @author Levi Costa																	*
+ * @copyright Copyright (c) 2014, Mercosul Network Devlopement							*
+ * @link http://www.voemercosul.com/network/br											*
+ * @license http://creativecommons.org/licenses/by/4.0/									*
  ***************************************************************************************/
  
  	class FeedData extends CodonData
